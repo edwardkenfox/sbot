8f0a0190c94108e70710897a83742c8688e186b9
alexkwa <alexkwa@me.com>
2015-10-23 01:09:00 +0900

    refactor(slim): convert to slim

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,53 @@
 module.exports = function(grunt) {
 
-        grunt.initConfig({
-                jshint: {
-                    files: ['Gruntfile.js', 'javascripts/size.js'],
-                    options: {
-                        globals: {
-                            jQuery: true
-                        }
-                    }
-                },
-                coffee: {
-                    compile: {
-                        files: {
-                            'javascripts/size.js': 'coffee/size.coffee',
-                            'javascripts/content.js': 'coffee/content.coffee'
-                        }
-                    }
-                },
-                    sass: {
-                        dist: {
-                            files: {
-                                'style.css': 'stylesheets/style.scss'
-                            }
-                        }
-                    },
-                    watch: {
-                        files: ['<%= jshint.files %>'],
-                        tasks: ['jshint'],
-                        sass: {
-                            files: 'sass/**/*.scss',
-                            tasks: ['sass']
-                        }
-                    }
-                });
+    grunt.initConfig({
+        jshint: {
+            files: ['Gruntfile.js', 'javascripts/size.js'],
+            options: {
+                globals: {
+                    jQuery: true
+                }
+            }
+        },
+        coffee: {
+            compile: {
+                files: {
+                    'javascripts/size.js': 'coffee/size.coffee',
+                    'javascripts/content.js': 'coffee/content.coffee'
+                }
+            }
+        },
+        slim: {
+            compile: {
+                files: {
+                    'popup-form.html': 'popup-form.html.slim',
+                    'popup-size.html': 'popup-size.html.slim',
+                    'popup-main.html': 'popup-main.html.slim'
+                }
+            }
+        },
+        sass: {
+            dist: {
+                files: {
+                    'style.css': 'stylesheets/style.scss'
+                }
+            }
+        },
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint'],
+            sass: {
+                files: 'sass/**/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
 
-            grunt.loadNpmTasks('grunt-contrib-jshint'); grunt.loadNpmTasks('grunt-contrib-watch'); grunt.loadNpmTasks('grunt-contrib-sass'); grunt.loadNpmTasks('grunt-contrib-coffee'); grunt.registerTask('default', ['jshint']); grunt.registerTask('default', ['sass']);
-
-        };
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-coffee');
+    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['sass']);
+    grunt.loadNpmTasks('grunt-slim');
+};
