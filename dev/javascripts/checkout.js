82bf81a541b7717a82b402486f12accecff2c159
Alex Kwa <alexkwa@me.com>
2016-06-17 16:57:44 +0900

    remove unneccesary

--- dev/javascripts/checkout.js
+++ dev/javascripts/checkout.js
@@ -12,44 +12,42 @@ $(function() {
 // Function to fill forms if on checkout page
 function fillforms() {
   var doc = window.document;
-  console.log("This is the check before a checkout page.");
-
-    console.log("This is a checkout page.");
-    var checkForm = setInterval(function () {
-      if (doc.getElementById("number_v")) {
-        clearInterval(checkForm);
-        var nowZone = doc.getElementById("time-zone-name").innerHTML;
-        if (nowZone === "TYO") {
-          console.log("This is the TYO checkout page.");
-          chrome.storage.local.get('tyoPref', function (items) {
-            var tyoPref = items.tyoPref;
-            for (var key in tyoPref) {
-              $("#" + key).val(tyoPref[key]);
-            }
-            if (tyoPref.credit_card_type === "cod") {
-              doc.getElementById("card_details").style.display = "none";
-            }
-          });
-        } else if (nowZone === "NYC") {
-          console.log("This is the NYC checkout page.");
-          chrome.storage.local.get('nycPref', function (items) {
-            var nycPref = items.nycPref;
-            for (var key in nycPref) {
-              $("#" + key).val(nycPref[key]);
-            }
-          });
-        } else {
-          console.log("Don't know which checkout page this is. Time zone text doesn't exist.");
-        }
-        //Check terms checkbox
-        doc.getElementById("order_terms").checked = true;
-        console.log("filledform");
-        chrome.storage.local.get('checkoutSwitch', function(items) {
-          console.log("this checkoutswich is " + items.checkoutSwitch)
-          if (items.checkoutSwitch == 1) {
-            $("input[name = 'commit']").click();
+  console.log("This is a checkout page.");
+  var checkForm = setInterval(function () {
+    if (doc.getElementById("number_v")) {
+      clearInterval(checkForm);
+      var nowZone = doc.getElementById("time-zone-name").innerHTML;
+      if (nowZone === "TYO") {
+        console.log("This is the TYO checkout page.");
+        chrome.storage.local.get('tyoPref', function (items) {
+          var tyoPref = items.tyoPref;
+          for (var key in tyoPref) {
+            $("#" + key).val(tyoPref[key]);
+          }
+          if (tyoPref.credit_card_type === "cod") {
+            doc.getElementById("card_details").style.display = "none";
           }
         });
+      } else if (nowZone === "NYC") {
+        console.log("This is the NYC checkout page.");
+        chrome.storage.local.get('nycPref', function (items) {
+          var nycPref = items.nycPref;
+          for (var key in nycPref) {
+            $("#" + key).val(nycPref[key]);
+          }
+        });
+      } else {
+        console.log("Don't know which checkout page this is. Time zone text doesn't exist.");
       }
-    }, 10);
+      //Check terms checkbox
+      doc.getElementById("order_terms").checked = true;
+      console.log("Form has been filled.");
+      chrome.storage.local.get('checkoutSwitch', function(items) {
+        console.log("The checkout switch is set at " + items.checkoutSwitch)
+        if (items.checkoutSwitch == 1) {
+          $("input[name = 'commit']").click();
+        }
+      });
+    }
+  }, 10);
 }
